a8091703383280225496c56abbed11d74a662a15
AAK <kekow@student.agh.edu.pl>
2023-01-22 10:59:20 +0100

    Poprawia działanie string

--- include/Calc.h
+++ include/Calc.h
@@ -9,37 +9,15 @@
 
 
 
-const Name kWelcomeString= "==========================================\n"
+const Name kWelcomeName= "==========================================\n"
 "Welcome to derivative calculator\n"
 "Written by Adam Andrzej Kek\xA2w 2023\n"
 "==========================================\n"
  ;
 
 void Run(void) {
-	/*
-	polynomial tmp,tmp2;
-	polynomial::Number_power_pair tmp3;
-	tmp2.other = "-sin(x)";
-	tmp.other = "-cos(x)";
-	tmp3.power = 7.0;
-	tmp3.number = 1.0;
-	tmp.poly.push_back(tmp3);
-	tmp3.power = 2.0;
-	tmp3.number = 3.0;
-	tmp.poly.push_back(tmp3);
-	tmp3.power = 0.0;
-	tmp3.number = 2.0;
-	tmp.poly.push_back(tmp3);
-	tmp3.power = 4.0;
-	tmp3.number = 1.0;
-	tmp2.poly.push_back(tmp3);
-	tmp3.power = 3.0;
-	tmp3.number = 7.0;
-	tmp2.poly.push_back(tmp3);
-	std::cout << tmp << std::endl << tmp2 << std::endl;
-	tmp = tmp / tmp2;
-	std::cout << tmp<<std::endl; */
-	std::cout << kWelcomeString;
+
+	std::cout << kWelcomeName;
 	while (true)
 	{
 		Name input;
